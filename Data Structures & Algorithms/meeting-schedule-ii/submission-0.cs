62bb8d6a00761b566ce6717a8320481429f6ee07
Tuan Bao Nguyen <134601844+TuanBaoNguyen2000@users.noreply.github.com>
2026-04-27 22:20:24 +0700

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.cs
@@ -0,0 +1,40 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int MinMeetingRooms(List<Interval> intervals) {
+        int n = intervals.Count;
+        int[] start = new int[n];
+        int[] end = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            start[i] = intervals[i].start;
+            end[i] = intervals[i].end;
+        }
+
+        Array.Sort(start);
+        Array.Sort(end);
+
+        int res = 0, count = 0, s = 0, e = 0;
+        while (s < n) {
+            if (start[s] < end[e]) {
+                s++;
+                count++;
+            } else {
+                e++;
+                count--;
+            }
+            res = Math.Max(res, count);
+        }
+
+        return res;
+    }
+}
