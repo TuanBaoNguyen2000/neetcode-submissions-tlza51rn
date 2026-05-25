37d682449a6cdc50c6723ad7ef8c0d1753b37725
Tuan Bao Nguyen <134601844+TuanBaoNguyen2000@users.noreply.github.com>
2026-05-26 00:45:06 +0700

    Add: partition-equal-subset-sum - submission-0

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-0.cs
@@ -0,0 +1,26 @@
+public class Solution 
+{
+    public bool CanPartition(int[] nums) 
+    {
+        int sum = nums.Sum();
+
+        if (sum % 2 != 0)
+            return false;
+
+        int target = sum / 2;
+
+        bool[] dp = new bool[target + 1];
+
+        dp[0] = true;
+
+        foreach (int num in nums)
+        {
+            for (int s = target; s >= num; s--)
+            {
+                dp[s] = dp[s] || dp[s - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
