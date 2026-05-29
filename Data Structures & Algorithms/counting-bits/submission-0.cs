a6e9a665dd4b8ded006e270b49d1d41e30da1774
Tuan Bao Nguyen <134601844+TuanBaoNguyen2000@users.noreply.github.com>
2026-05-30 00:23:00 +0700

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] CountBits(int n) {
+
+        int[] res = new int[n + 1];
+
+        for (int i = 1; i <= n; i++)
+        {
+            res[i] = res[i & (i - 1)] + 1;
+        }
+
+        return res;
+    }
+}
